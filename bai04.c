02ba4d1fa2e0d87ddff2f52054bf89b5b23956d0
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2021-09-29 13:59:17 +0700

    Add files via upload

---
+++ bai04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int c;
+	printf("nhap vao do C");
+	scanf("%d", &c);
+	printf("chuyen thanh %d do F", c/5*9+32);
+	
+	
+	return 0;
+}
